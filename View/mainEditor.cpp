02aab677d44263d24330d9e6ff54062a11cb97a9
dcassinotti <damiancassinotti.33@gmail.com>
2017-11-09 18:07:32 -0300

    Refactor del loop del editor

--- View/mainEditor.cpp
+++ View/mainEditor.cpp
@@ -42,41 +42,42 @@ int main(int argc, char** argv) {
     int tileY = 0;
 
     while (!quit) {
-        SDL_PollEvent(&event);
 
-        switch (event.type) {
-            case SDL_QUIT:
-                quit = true;
-                break;
-            case SDL_MOUSEBUTTONDOWN:
-                SDL_GetMouseState(&mouse_x, &mouse_y);
-                mouse.activate();
-                break;
-            case SDL_FINGERDOWN:
-                mouse_x = static_cast<int>(event.tfinger.x);
-                mouse_y = static_cast<int>(event.tfinger.y);
-                mouse.activate();
-                break;
-            case SDL_KEYDOWN:
-                switch (event.key.keysym.sym) {
-                    case SDLK_ESCAPE: quit = true; break;
-                    case SDLK_PLUS: renderer.zoomIn(); break;
-                    case SDLK_MINUS: renderer.zoomOut(); break;
-                    case SDLK_LEFT:  renderer.updateCamera(-1, 0); break;
-                    case SDLK_RIGHT: renderer.updateCamera(1, 0); break;
-                    case SDLK_UP:    renderer.updateCamera(0, -1); break;
-                    case SDLK_DOWN:  renderer.updateCamera(0, 1); break;
-                }
-        }
-        if (mouse.isActive()) {
-            if (!buttons.isAnyClicked()) {
-                tileX = mapView.getTileXFromPixel(mouse_x, mouse_y);
-                tileY = mapView.getTileYFromPixel(mouse_x, mouse_y);
-                editor.applyTileFunction(tileX, tileY);
-                mouse.deactivate();
+        while(SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_QUIT:
+                    quit = true;
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    SDL_GetMouseState(&mouse_x, &mouse_y);
+                    mouse.activate();
+                    break;
+                case SDL_FINGERDOWN:
+                    mouse_x = static_cast<int>(event.tfinger.x);
+                    mouse_y = static_cast<int>(event.tfinger.y);
+                    mouse.activate();
+                    break;
+                case SDL_KEYDOWN:
+                    switch (event.key.keysym.sym) {
+                        case SDLK_ESCAPE: quit = true; break;
+                        case SDLK_PLUS: renderer.zoomIn(); break;
+                        case SDLK_MINUS: renderer.zoomOut(); break;
+                        case SDLK_LEFT:  renderer.updateCamera(-1, 0); break;
+                        case SDLK_RIGHT: renderer.updateCamera(1, 0); break;
+                        case SDLK_UP:    renderer.updateCamera(0, -1); break;
+                        case SDLK_DOWN:  renderer.updateCamera(0, 1); break;
+                    }
             }
+            if (mouse.isActive()) {
+                if (!buttons.isAnyClicked()) {
+                    tileX = mapView.getTileXFromPixel(mouse_x, mouse_y);
+                    tileY = mapView.getTileYFromPixel(mouse_x, mouse_y);
+                    editor.applyTileFunction(tileX, tileY);
+                    mouse.deactivate();
+                }
+            }
+            event.type = 0;
         }
-        event.type = 0;
         renderer.clearRender();
         editor.draw();
         buttons.draw();
