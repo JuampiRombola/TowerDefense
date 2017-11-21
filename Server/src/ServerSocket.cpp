c6f969525965cc86c401c202d44efe14a872da63
martinqueija <tinqueija@gmail.com>
2017-11-21 08:46:29 -0300

    agrego AI_PASSIVE a los flags de getaddrinfo para bindear on all available interfaces, para poder jugar en lan :)

--- Server/src/ServerSocket.cpp
+++ Server/src/ServerSocket.cpp
@@ -26,7 +26,7 @@ ServerSocket::ServerSocket(std::string service) : SocketWrapper(-1), _listening(
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = 0;
+	hints.ai_flags = AI_PASSIVE;
 	error = getaddrinfo(NULL, service.c_str(), &hints, &server_info);
 	if (error != 0){
 		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(error));
