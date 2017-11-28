befe94895a03ea4cb88f47252450916261b42a36
Martin Queija <tinqueija@gmail.com>
2017-11-28 02:52:53 -0300

    loop a gtk despues de sdl

--- Server/src/GameModel/Towers/Tower.cpp
+++ Server/src/GameModel/Towers/Tower.cpp
@@ -7,6 +7,7 @@
 #include "../../../include/GameModel/Map/Map.h"
 #include "../../../include/GameModel/Helpers.h"
 #include "../../../include/GameModel/GameNotifications/TowerGainedExperienceGameNotification.h"
+#include "../../../include/GameModel/Map/PathTile.h"
 
 Tower::Tower(uint cooldown_ms, uint range, uint damage, SolidGroundTile* pos, Map* map, uint projectile_ms_over_tile, ThreadSafeQueue<GameNotification*>& notifications) :
 _lastTimeStamp_ms(0), _cooldown_ms(cooldown_ms), _map(map), _position(pos), _range(range), _experience(0), _damage(damage),
@@ -47,18 +48,32 @@ EnviormentUnit* Tower::_GetTargetUnitInRange(TowerType type){
 	if (units.size() == 0)
 		return nullptr;
 
-	//De todos los que tengo en rango agarro el primero que encuentro.
-	if (type != Ground)
-		return *(units.begin());
-	else{
-		for (auto it = units.begin(); it != units.end(); ++it){
+	if (type == Ground){
+		for (auto it = units.begin(); it != units.end();){
 			EnviormentUnit* unit = *it;
-			if (!unit->Flies())
-				return unit;
+			if (unit->Flies())
+				it = units.erase(it);
+			else
+				++it;
 		}
 	}
 
-	return nullptr;
+
+	EnviormentUnit* closestUnit = nullptr;
+	double mindistance = 100000;
+	for (auto it = units.begin(); it != units.end(); ++it){
+		EnviormentUnit* unit = *it;
+		PathTile* tile = unit->GetPosition();
+		uint deltax = abs(tile->GetXPos() - _position->GetXPos());
+		uint deltay = abs(tile->GetYPos() - _position->GetYPos());
+		double distance = sqrt(pow(deltax, 2) + pow(deltay, 2));
+		if (distance < mindistance){
+			mindistance = distance;
+			closestUnit = unit;
+		}
+	}
+
+	return closestUnit;
 }
 
 
